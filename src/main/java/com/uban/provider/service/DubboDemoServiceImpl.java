c1e08e80c8e85ee3efd13db43ca6495bb2e034b2
DuanYangYu <DuanYangYu@uban.com>
2017-02-20 17:15:49 +0800

    添加rest请求支持

--- src/main/java/com/uban/provider/service/DubboDemoServiceImpl.java
+++ src/main/java/com/uban/provider/service/DubboDemoServiceImpl.java
@@ -1,15 +1,24 @@
 package com.uban.provider.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
 import com.alibaba.fastjson.JSONObject;
 import com.uban.provider.dao.DubboDemoDao;
 import com.uban.service.DubboDemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
 /**
  * Created by DuanYangYu on 2016/11/2 0002.
+ * @Path 类似springMVC中的RequestMapping
  */
+@Path("demo")
 @Service
+@Produces({ ContentType.APPLICATION_JSON_UTF_8 })
 public class DubboDemoServiceImpl implements DubboDemoService {
 
     @Autowired
@@ -21,6 +30,9 @@ public class DubboDemoServiceImpl implements DubboDemoService {
         return jo;
     }
 
+    @GET
+    @POST
+    @Path("users")
     public JSONObject getUsers() {
         JSONObject jo = new JSONObject();
         jo.put("result",dubboDemoDao.getUsers());
